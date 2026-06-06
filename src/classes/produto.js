b4c7fbf74f4de6cf7f430db823a53553505d37f8
JoseVitorDevMoment <josevitortrindadebarbosa@gmail.com>
2026-06-06 20:52:46 -0300

    Feat: Adicionado: produto.js

---
+++ src/classes/produto.js
@@ -0,0 +1,8 @@
+class Produto {
+  constructor(id, nome, descricao, quantidade) {
+    this.id = id;
+    this.nome = nome;
+    this.descricao = descricao;
+    this.quantidade = quantidade;
+  }
+}
